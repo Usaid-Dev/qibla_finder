706abc33378107bf3b898e38a0ace029e7fedc8b
Usaid Asif <usaidasif12@gmail.com>
2022-12-20 14:41:03 +0500

    Created qiblah finder screen | added permissions

--- lib/Screens/qiblah_screen.dart
+++ lib/Screens/qiblah_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_qiblah/flutter_qiblah.dart';
 
 class QiblahScreen extends StatefulWidget {
   const QiblahScreen({super.key});
@@ -7,9 +10,61 @@ class QiblahScreen extends StatefulWidget {
   State<QiblahScreen> createState() => _QiblahScreenState();
 }
 
-class _QiblahScreenState extends State<QiblahScreen> {
+Animation<double>? animation;
+AnimationController? _animationController;
+double begin = 0.0;
+
+class _QiblahScreenState extends State<QiblahScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    animation = Tween(begin: 0.0, end: 0.0).animate(_animationController!);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: StreamBuilder(
+          stream: FlutterQiblah.qiblahStream,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Container(
+                alignment: Alignment.center,
+                child: const CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              );
+            }
+
+            final qiblahDirection = snapshot.data;
+            animation = Tween(
+                    begin: begin,
+                    end: (qiblahDirection!.qiblah * (pi / 180) * -1))
+                .animate(_animationController!);
+            begin = (qiblahDirection.qiblah * (pi / 180) * -1);
+            _animationController!.forward(from: 0);
+
+            return Center(
+              child: SizedBox(
+                child: AnimatedBuilder(
+                  animation: animation!,
+                  builder: (context, child) => Transform.rotate(
+                    angle: animation!.value,
+                    child: Image.asset(
+                      'images/qibla_image.png',
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:qibla_finder/Screens/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool hasPermission = false;
+
+  Future getPermission() async {
+    if (await Permission.location.serviceStatus.isEnabled) {
+      var status = await Permission.location.status;
+      if (status.isGranted) {
+        hasPermission = true;
+      } else {
+        Permission.location.request().then((value) {
+          setState(() {
+            hasPermission = (value == PermissionStatus.granted);
+          });
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +38,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const SplashScreen(),
+      home: FutureBuilder(
+        builder: (context, snapshot) {
+          if (hasPermission) {
+            return const SplashScreen();
+          } else {
+            return Scaffold(backgroundColor: Colors.blueGrey.shade800);
+          }
+        },
+        future: getPermission(),
+      ),
     );
   }
 }
